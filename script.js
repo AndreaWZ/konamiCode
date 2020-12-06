9e14f2a31009a51eff7caead8625878a11245d7c
Andrea Wu <lixianhyun@gmail.com>
2020-12-05 19:52:03 -0800

    add cornify's ulr and add it to JS file

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ window.onload = function() {
 
     window.addEventListener("keyup", function(e){
         pressed.push(e.key);
-        console.log(pressed);
         pressed.splice(-secretCode.lenght -1, pressed.length - secretCode.length);
 
         if(pressed.join('').includes(secretCode)){
-            console.log("DING, DING!!!");
+            cornify_add();
         }
     })
 }
