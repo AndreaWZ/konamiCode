e9a9284a3dd19d22d1a04fc94daba482fc239137
Andrea Wu <lixianhyun@gmail.com>
2020-12-05 18:04:44 -0800

    splice 'pressed' array length to 'secretCode' variable lenght, and join 'pressed' array and included 'secretCode' to it

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.onload = function() {
 
     const pressed = [];
-    const secretCode = 'secret';
+    const secretCode = 'konami';
 
     window.addEventListener("keyup", function(e){
-        console.log(e.key);
+        pressed.push(e.key);
+        console.log(pressed);
+        pressed.splice(-secretCode.lenght -1, pressed.length - secretCode.length);
+
+        if(pressed.join('').includes(secretCode)){
+            console.log("DING, DING!!!");
+        }
     })
 }
